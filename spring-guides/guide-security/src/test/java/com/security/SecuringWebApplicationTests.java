eeef6be5faade7a14718c529c0a563448a3adac6
Rohit Nandi <rohitnandi12@gmail.com>
2024-07-19 23:26:33 +0530

    Adds spring-guide-security

---
+++ spring-guides/guide-security/src/test/java/com/security/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
