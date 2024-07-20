41a74a4ac56102c3e278c9ab56079779a8429981
Rohit Nandi <rohitnandi12@gmail.com>
2024-07-20 19:33:11 +0530

    Adds guide-rest-service

---
+++ spring-guides/guide-rest-service/src/main/java/com/guide_rest_service/GuideRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.guide_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideRestServiceApplication.class, args);
+	}
+
+}
---
+++ spring-guides/guide-rest-service/src/test/java/com/guide_rest_service/GuideRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
