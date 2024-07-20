c450b70ef66c61f4984b5195460adf73382ed4f1
Rohit Nandi <rohit.nandi@gmail.com>
2024-07-20 19:48:07 +0530

    Adds guide-scheduling-tasks

---
+++ spring-guides/guide-scheduling-tasks/src/test/java/com/guide_scheduling_tasks/GuideSchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide_scheduling_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideSchedulingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
