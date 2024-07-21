8e1b1f2f1a984cd41c18039366d910798527cfe2
Rohit Nandi <rohitnandi12@gmail.com>
2024-07-21 15:57:54 +0530

    Adds guide-uploading-files

---
+++ spring-guides/guide-uploading-files/src/test/java/com/guide_uploading_files/GuideUploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide_uploading_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideUploadingFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
