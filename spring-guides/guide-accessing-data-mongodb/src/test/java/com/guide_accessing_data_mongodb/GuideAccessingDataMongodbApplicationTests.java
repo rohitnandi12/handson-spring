b708349150a92de9f71d77f39724104f3828440a
rohitnandi12 <rohitnandi12@gmail.com>
2024-10-05 12:56:13 +0530

    spring-guide:spring-data-mongodb

---
+++ spring-guides/guide-accessing-data-mongodb/src/test/java/com/guide_accessing_data_mongodb/GuideAccessingDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide_accessing_data_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideAccessingDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
